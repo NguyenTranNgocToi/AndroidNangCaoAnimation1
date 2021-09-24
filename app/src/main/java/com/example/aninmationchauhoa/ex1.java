7ea97960bfd52e8c85b7dee3359fbb33b16baee7
nguyentoi53@gmail.com <nguyentoi53@gmail.com>
2021-09-24 17:36:16 +0700

    lỗi file đã sửa

--- app/src/main/java/com/example/aninmationchauhoa/ex1.java
+++ app/src/main/java/com/example/aninmationchauhoa/ex1.java
@@ -12,13 +12,5 @@ public class ex1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ex1);
-
-        findViewById(R.id.button_Go).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), ex2.class));
-                overridePendingTransition(R.anim.enter, R.anim.exit);
-            }
-        });
     }
 }
